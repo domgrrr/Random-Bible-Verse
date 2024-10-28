8571c0a5ea2d0b28829cea924ea9bf19808ad914
Dominique Guerrero <domguerr@hotmail.com>
2024-10-28 12:13:41 -0600

    JS

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API_KEY = "ef58a648aa506d7fdee18f9e119eeb63";
 const BIBLE_ID = "9879dbb7cfe39e4d-01";
 const API_BASE_URL = "https://api.scripture.api.bible/v1";
 
-const versHistory = [];
+const verseHistory = [];
 const maxHistoryItems = 5;
 
 //  Initialize event listeners
