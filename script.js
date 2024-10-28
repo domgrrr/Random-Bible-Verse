2c95fd3a5053c8ed315ab0e6b9cb94d99577b1af
Dominique Guerrero <domguerr@hotmail.com>
2024-10-28 11:56:22 -0600

    JS

--- script.js
+++ script.js
@@ -15,5 +15,8 @@ function showToast(message) {
   const toast = document.getElementById('toast');
   toast.textContent = message;
   toast.style.display = 'block';
-  
+  setTimeout(() => {
+    toast.style.display = 'none';
+  }, 3000);
 }
+
