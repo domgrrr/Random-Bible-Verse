43c460e6c24243bc88af02f017f9a4e1d7dc0c40
Dominique Guerrero <domguerr@hotmail.com>
2024-10-29 13:44:27 -0600

    King James Update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_KEY = "ef58a648aa506d7fdee18f9e119eeb63";
-const BIBLE_ID = "9879dbb7cfe39e4d-01";
+const BIBLE_ID = "de4e12af7f28f599-01";
 const API_BASE_URL = "https://api.scripture.api.bible/v1";
 
 const verseHistory = [];
