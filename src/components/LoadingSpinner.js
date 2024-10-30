ab3ec69f65dd5673b46fa4e4672c2a173c0dba71
Dominique Guerrero <domguerr@hotmail.com>
2024-10-29 22:13:34 -0600

    Loading Spinner

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,17 @@
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+export const LoadingSpinner = styled.div`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 3px solid #f3f3f3;
+  border-top: 3px solid #4caf50;
+  border-radius: 50%;
+  animation: ${spin} 1s linear infinite;
+  margin-right: 10px;
+`;
